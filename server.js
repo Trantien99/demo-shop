e5beaa11d0e9345ccfb13eec3207b2528c3a9111
tranv <tranviettien0604@gmail.com>
2024-03-23 15:35:22 +0700

    set config build

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const app = express();
 
-const forceSSL = function () {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-};
-app.use(express.static('./dist/demo-shop'));
+// const forceSSL = function () {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//         ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// };
+app.use(express.static(__dirname+'./dist/demo-shop'));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname,'/dist/demo-shop/index.html'));
 });
 
-app.use(forceSSL());
+// app.use(forceSSL());
 
 app.listen(process.env.PORT || 8080);
